eb2c5a7de40a9294baffe496a206a9ac9fba1747
iaramer <yaramer95@gmail.com>
2021-08-07 18:32:09 +0300

    Fixed answers

--- main.py
+++ main.py
@@ -9,21 +9,16 @@ TOKEN = os.getenv('TOKEN')
 bot = telebot.AsyncTeleBot(TOKEN)
 
 text_messages = {
-    'welcome':
+    'start':
         '''
-        Hi!
-        I am AIAdvisor bot.
-        You can ask me about USD/RUB and EUR/RUB currency pairs and BTC/USD, ETH/USD cryptocurrencies.
-        Use the following commands: 
-        /help - shows help;
-        /go - shows prices.
+        Hi! I am AIAdvisor bot.
         ''',
 
     'info':
         '''
-        You can ask me about USD/RUB and EUR/RUB currency pairs and BTC/USD, ETH/USD cryptocurrencies.
-        Use the following commands: 
-        /help - shows help;
+        You can ask me about USD/RUB and EUR/RUB currency pairs and BTC/USD, ETH/USD cryptocurrencies.\n
+        Use the following commands:\n
+        /help - shows help;\n
         /go - shows prices.
         ''',
 
@@ -34,7 +29,8 @@ text_messages = {
 
 @bot.message_handler(commands=['start'])
 def on_start(message):
-    bot.reply_to(message, text_messages['welcome'])
+    bot.reply_to(message, text_messages['start'])
+    on_info(message)
 
 
 @bot.message_handler(commands=['info', 'help'])
@@ -54,7 +50,7 @@ def on_go(message):
     price_usd = "{:,}".format(round(1 / float(response_btc.json()['rates']['USD']), 2))
     price_eur = "{:,}".format(round(1 / float(response_btc.json()['rates']['EUR']), 2))
 
-    prices = text_messages['go'].format(price_btc, price_eth, price_usd, price_eur)
+    prices = text_messages['go'].format(price_usd, price_eur, price_btc, price_eth)
     bot.send_message(message.chat.id, prices)
 
 
@@ -62,11 +58,10 @@ def on_go(message):
 def on_any_message(message):
     if message.text.lower() in ['go', 'currency', 'btc', 'eth', 'rub', 'usd', 'eur']:
         on_go(message)
-    elif message.text.lower() in ['info', help]:
+    elif message.text.lower() in ['info', 'help']:
         on_info(message)
     else:
-        bot.reply_to(message, 'Wooo! Here should be some ML')
-        bot.send_message(message.chat.id, 'For now I can answer only fixed set of commands')
+        bot.reply_to(message, 'Wooo! Here should be some ML.\nFor now I can answer only fixed set of commands :(')
         bot.send_message(message.chat.id, text_messages['info'])
 
 
---
+++ readme.md
@@ -0,0 +1,21 @@
+# AIAdvisor bot
+
+Telegram bot for getting prices of currencies.
+
+## Deploy to Heroku
+
+Procfile describes deploy to Heroku.
+
+To connect repository to heroku use the following command:
+
+```
+heroku git:remote -a aiadvisor-bot
+```
+
+Push your code to Heroku with the following command:
+
+```
+git push heroku master
+```
+
+After that Heroku should automatically deploy the app.
